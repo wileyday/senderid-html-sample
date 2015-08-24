065f4527c4a439069c165fdd10e79ee736427e88
wileyday <wileychoi@gmail.com>
2015-08-24 18:22:28 +0900

    SenderID API 1.1에 맞게 업데이트

--- register.php
+++ register.php
@@ -7,18 +7,17 @@ $salt = uniqid();
 $data = strval($timestamp) . $salt;
 $signature = hash_hmac('md5', $data, $api_secret);
 ?>
-<html lang="ko" xml:lang="ko" xmlns="http://www.w3.org/1999/xhtml">
+<html lang="ko">
         <head>
                 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         </head>
         <body>
-                <form method="post" action="http://rest1.coolsms.co.kr/senderid/1/register">
+                <form method="post" action="https://api.coolsms.co.kr/senderid/1.1/register">
                         API Key: <input type="text" name="api_key" value="<?php echo $api_key ?>" /><br />
                         Timestamp: <input type="text" name="timestamp" value="<?php echo $timestamp ?>" /><br />
                         Salt: <input type="text" name="salt" value="<?php echo $salt ?>" /><br />
                         Signature: <input type="text" name="signature" value="<?php echo $signature ?>" /><br />
                         Phone number: <input type="text" name="phone" value="" /><br />
-                        Verification code: <input type="text" name="code" value="" /><br />
                         <input type="submit" value="Submit" />
                 </form>
         </body>
