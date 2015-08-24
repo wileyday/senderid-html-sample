fdf9364f755a9e6876ee94938dda291e4b3561a0
wileyday <wileychoi@gmail.com>
2015-08-24 18:23:13 +0900

    누락파일 추가

---
+++ get_default.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<?php
+$api_key = '__YOUR_API_KEY__';
+$api_secret = '__YOUR_API_SECRET__';
+$timestamp = time();
+$salt = uniqid();
+$data = strval($timestamp) . $salt;
+$signature = hash_hmac('md5', $data, $api_secret);
+?>
+<html lang="ko">
+        <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        </head>
+        <body>
+                <form method="get" action="https://api.coolsms.co.kr/senderid/1.1/get_default">
+                        API Key: <input type="text" name="api_key" value="<?php echo $api_key ?>" /><br />
+                        Timestamp: <input type="text" name="timestamp" value="<?php echo $timestamp ?>" /><br />
+                        Salt: <input type="text" name="salt" value="<?php echo $salt ?>" /><br />
+                        Signature: <input type="text" name="signature" value="<?php echo $signature ?>" /><br />
+                        Site User(option): <input type="text" name="site_user" value="" /><br />
+                        <input type="submit" value="Submit" />
+                </form>
+        </body>
+</html>
---
+++ verify.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<?php
+$api_key = '__YOUR_API_KEY__';
+$api_secret = '__YOUR_API_SECRET__';
+$timestamp = time();
+$salt = uniqid();
+$data = strval($timestamp) . $salt;
+$signature = hash_hmac('md5', $data, $api_secret);
+?>
+<html lang="ko">
+        <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        </head>
+        <body>
+                <form method="post" action="https://api.coolsms.co.kr/senderid/1.1/verify">
+                        API Key: <input type="text" name="api_key" value="<?php echo $api_key ?>" /><br />
+                        Timestamp: <input type="text" name="timestamp" value="<?php echo $timestamp ?>" /><br />
+                        Salt: <input type="text" name="salt" value="<?php echo $salt ?>" /><br />
+                        Signature: <input type="text" name="signature" value="<?php echo $signature ?>" /><br />
+                        Handle Key: <input type="text" name="handle_key" value="" /><br />
+                        <input type="submit" value="Submit" />
+                </form>
+        </body>
+</html>
